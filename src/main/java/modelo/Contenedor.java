f172b0b2efafa0542d64badbc8d0ba71bdfd7522
Marcos Zambrano <marcos.d.zambrano@gmail.com>
2026-07-08 21:41:23 -0500

    feat: clase Contenedor con constructores sobrecargados

---
+++ src/main/java/modelo/Contenedor.java
@@ -0,0 +1,35 @@
+package modelo;
+
+public class Contenedor {
+    protected double capacidadMaxima;
+    protected double nivelActual;
+
+    // Constructor para un contenedor nuevo, vacío
+    public Contenedor(double capacidadMaxima) {
+        this(capacidadMaxima, 0); // reutiliza el otro constructor
+    }
+
+    // Constructor para restaurar un contenedor con un nivel ya existente (persistencia)
+    public Contenedor(double capacidadMaxima, double nivelActual) {
+        this.capacidadMaxima = capacidadMaxima;
+        this.nivelActual = nivelActual;
+    }
+
+    public void agregarResiduo(double pesoResiduo) {
+        double nuevoPeso = nivelActual + pesoResiduo;
+        if (nuevoPeso > capacidadMaxima) {
+            throw new IllegalArgumentException("Has excedido la capacidad máxima del contenedor");
+        }
+
+        nivelActual = nuevoPeso;
+    }
+
+    public double getPorcentajeLlenado() {
+        double porcentaje = (nivelActual / capacidadMaxima) * 100;
+        return porcentaje;
+    }
+
+    public void vaciar() {
+        nivelActual = 0;
+    }
+}
